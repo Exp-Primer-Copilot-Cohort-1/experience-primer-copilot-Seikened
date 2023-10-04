ba67e71df3f2bd4f3c185e74c01a921e079f5344
Seikened <91928331+Seikened@users.noreply.github.com>
2023-10-04 05:37:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember()
+{
+    var skills = ["HTML", "CSS", "JS", "PHP"];
+    var member = {
+        name: "John",
+        age: 25,
+        skills: skills,
+        address: {
+            street: "5th Avenue",
+            number: 123,
+            city: "New York"
+        }
+    };
+    console.log(member.skills[2]);
+    console.log(member.address.city);
+}
+
